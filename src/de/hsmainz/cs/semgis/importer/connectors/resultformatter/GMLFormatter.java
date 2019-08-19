a33cc08fc51f0a263782367353a49a6098b601e0
situx <timo.homburg@gmx.de>
2019-08-19 22:28:41 +0200

    Formatter work

--- src/de/hsmainz/cs/semgis/importer/connectors/resultformatter/GMLFormatter.java
+++ src/de/hsmainz/cs/semgis/importer/connectors/resultformatter/GMLFormatter.java
@@ -1,5 +1,40 @@
 package de.hsmainz.cs.semgis.importer.connectors.resultformatter;
 
-public class GMLFormatter {
+import java.io.StringWriter;
+import java.util.List;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+
+public class GMLFormatter extends ResultFormatter {
+
+	@Override
+	public String formatter(ResultSet results) throws XMLStreamException {
+		XMLOutputFactory factory = XMLOutputFactory.newInstance();
+		StringWriter strwriter=new StringWriter();
+		XMLStreamWriter writer=factory.createXMLStreamWriter(strwriter);
+		writer.writeStartDocument();
+		writer.writeStartElement("member");
+		List<QuerySolution> test=ResultSetFormatter.toList(results);
+	    for(QuerySolution solu:test) {
+			writer.writeStartElement("classname");
+			while(solu.varNames().hasNext()) {
+				String curvar=solu.varNames().next();
+				writer.writeStartElement(curvar);
+				writer.writeCharacters(solu.get("curvar").toString());
+				writer.writeEndElement();
+			}
+			writer.writeEndElement();    	
+	    }
+		writer.writeEndElement();
+		writer.writeEndDocument();
+		writer.flush();	
+		return strwriter.toString();
+	}
 
 }
--- src/de/hsmainz/cs/semgis/importer/connectors/resultformatter/ResultFormatter.java
+++ src/de/hsmainz/cs/semgis/importer/connectors/resultformatter/ResultFormatter.java
@@ -2,6 +2,9 @@ package de.hsmainz.cs.semgis.importer.connectors.resultformatter;
 
 import java.util.Map;
 import java.util.TreeMap;
+
+import javax.xml.stream.XMLStreamException;
+
 import org.apache.jena.query.ResultSet;
 
 public abstract class ResultFormatter {
@@ -12,7 +15,7 @@ public abstract class ResultFormatter {
 		resultMap.put("geojson", new GeoJSONFormatter());	
 	}
 
-	public abstract String formatter(ResultSet results);
+	public abstract String formatter(ResultSet results) throws XMLStreamException;
 
 	public String formatHeader() {
 		return "";
