418a2fc32cb25cb91a4b21c2f9a115ac16f9ead2
Timo Homburg <timo.homburg@hs-mainz.de>
2020-01-06 22:08:47 +0100

    Update HTMLFormatter.java

--- src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/HTMLFormatter.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/HTMLFormatter.java
@@ -1,37 +1,62 @@
 package de.hsmainz.cs.semgis.wfs.resultformatter;
 
-import java.util.Iterator;
-
 import javax.xml.stream.XMLStreamException;
 
-import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class HTMLFormatter extends ResultFormatter {
 
 	@Override
 	public String formatter(ResultSet results) throws XMLStreamException {
+		JSONObject geojson=new JSONObject(resultMap.get("geojson").formatter(results));
+		System.out.println(geojson);
 		StringBuilder builder=new StringBuilder();
+		builder.append("<script>var overlayMaps={}; var overlayControl; var markercollection=[];var geojson="+geojson.toString()+"</script>");
+		builder.append("<div id=\"mapid\" style=\"height: 500px;\"><script>var map = L.map('mapid',{fullscreenControl: true,fullscreenControlOptions: {position: 'topleft'}}).setView([51.505, -0.09], 13); var layer=L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'});");
+		builder.append("var baseMaps = {\"OSM\": layer}; baseMaps[\"OSM\"].addTo(map);" + 
+				"	L.control.scale({\r\n" + 
+				"	position: 'bottomright',\r\n" + 
+				"	imperial: false\r\n" + 
+				"}).addTo(map);" + 
+				"var layercontrol=L.control.layers(baseMaps,overlayMaps).addTo(map);");
+		builder.append("var geojsonlayer=L.geoJSON(geojson, {coordsToLatLng: function (coords) {return new L.LatLng(coords[0], coords[1], coords[2]);},"+
+				"		onEachFeature: function (feature, layer) {\r\n" + 
+				"	 var popup=\"Official Data<br/><ul>\"\r\n" + 
+				"	 for(prop in feature.properties){\r\n" + 
+				"	 	popup+=\"<li>\"+prop+\" - \"+feature.properties[prop]+\"</li>\"\r\n" + 
+				"	 }\r\n" + 
+				"	 popup+=\"</ul>\"\r\n" + 
+				"	 console.log(feature)\r\n" + 
+				"         layer.bindPopup(popup);\r\n" + 
+				"     }}).addTo(map);map.fitBounds(geojsonlayer.getBounds());</script></div>");
 		builder.append("<table width=\"100%\" align=\"center\" id=\"queryres\" class=\"tablesorter\" border=\"1\"><tr>");
 		Boolean first=true;
-		while(results.hasNext()) {
-			QuerySolution solu=results.next();
+		JSONArray features=geojson.getJSONArray("features");
+		for(int i=0;i<features.length();i++) {
 			if(first) {
-				Iterator<String> iter=solu.varNames();
 				builder.append("<thead><tr>");
-				while(iter.hasNext()) {
-					builder.append("<th>"+iter.next()+"</th>");
+				for(String key:features.getJSONObject(0).getJSONObject("properties").keySet()) {
+					builder.append("<th>"+key+"</th>");
 				}
 				builder.append("</tr></thead><tbody>");
 				first=false;
 			}
-			Iterator<String> iter=solu.varNames();
-			while(iter.hasNext()) {
-				builder.append("<td align=\"center\">"+solu.get(iter.next())+"</td>");
+			System.out.println(builder.toString());
+			for(String key:features.getJSONObject(i).getJSONObject("properties").keySet()) {
+				System.out.println(key);
+				String value=features.getJSONObject(i).getJSONObject("properties").get(key).toString();
+				if(value.contains("http")) {
+					builder.append("<td align=\"center\"><a href=\""+value+"\" target=\"_blank\">"+value+"</a></td>");
+				}else {
+					builder.append("<td align=\"center\">"+value+"</td>");
+				}
 			}
 			builder.append("</tr>");
-	    }
+		}
 		builder.append("</tbody></table>");
+		System.out.println(builder.toString());
 		return builder.toString();
 	}
 
