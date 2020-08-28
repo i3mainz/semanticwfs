b641ac70ab7ee2ddfdef52c392ef898b8ca89b1e
situx <timo.homburg@gmx.de>
2020-08-29 00:32:32 +0200

    Import link fix

--- src/main/java/de/hsmainz/cs/semgis/wfs/webservice/WebService.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/webservice/WebService.java
@@ -1421,9 +1421,9 @@ public class WebService {
 					"</li></ul>Local Options:<ul><li><a href=\""+ wfsconf.getString("baseurl")+"/config/geotreeview.html\">Ontology Browser</a></li>"
 							+ "<li><a href=\""+wfsconf.getString("baseurl")+"/config/queryinterface.html\">SPARQL Query Interface</a></li>"
 							+ "<li><a href=\""+ wfsconf.getString("baseurl")
-							+ "/config/configuration.html\">Semantic WFS Configuration</a></li><li><a href=\"https://www.i3mainz.de/projekte/semgis/gmlimporter/\">Semantic Uplift Tools</a></li></ul></div></div></div><footer id=\"footer\"><table width=100%><tbody><tr><td><a href=\"" + wfsconf.getString("baseurl")
+							+ "/config/configuration.html\">Semantic WFS Configuration</a></li><li><a href=\"../importer/\">Semantic Uplift Tools</a></li></ul></div></div></div><footer id=\"footer\"><table width=100%><tbody><tr><td><a href=\"" + wfsconf.getString("baseurl")
 					+ "/?f=html\">Back to LandingPage</a></td><td align=right>This page in <a href=\""
-					+ wfsconf.getString("baseurl") + "/?f=gml\">[XML]</a> <a href=\""
+					+ wfsconf.getString("baseurl") + "/?f=xml\">[XML]</a> <a href=\""
 					+ wfsconf.getString("baseurl")
 					+ "/?f=json\">[JSON]</a></td></tr></table></footer></body></html>");
 			return Response.ok(builder.toString()).type(MediaType.TEXT_HTML).build();
