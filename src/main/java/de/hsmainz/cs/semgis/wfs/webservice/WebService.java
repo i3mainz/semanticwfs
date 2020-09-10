6cfe9d1ba271c72c8db24be3e1629a2efc9e9603
timo.homburg <timo.homburg@hs-mainz.de>
2020-09-10 12:17:24 +0200

    JSONSeq fix

--- src/main/java/de/hsmainz/cs/semgis/wfs/webservice/WebService.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/webservice/WebService.java
@@ -2144,7 +2144,7 @@ public class WebService {
 				throw new NotFoundException();
 			}
 			// System.out.println(res);
-			if (format != null && format.contains("json") && !format.equalsIgnoreCase("rdfjson")) {
+			if (format != null && format.contains("json") && !format.contains("seq") && !format.equalsIgnoreCase("rdfjson")) {
 				JSONObject result = new JSONObject();
 				JSONArray links = new JSONArray();
 				for (ResultFormatter formatter : ResultFormatter.resultMap.values()) {
