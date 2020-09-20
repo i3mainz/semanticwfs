4d68eed79b5a45f1035689dc768a360ee9d85bc9
situx <timo.homburg@gmx.de>
2020-09-21 00:33:33 +0200

    More ASCII Grid support work

--- src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/CoverageResultFormatter.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/CoverageResultFormatter.java
@@ -128,17 +128,37 @@ public abstract class CoverageResultFormatter extends ResultFormatter {
 			result=generateCOVJSONForXY("Grid");
 			param="altitude";
 			result=addCovJSONParameterAndRange(result, param, "http://www.opengis.net/ont/geosparql#altitude", new LinkedList<>(), "float");
-			xarray=result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").getJSONArray("values");
-			yarray=result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").getJSONArray("values");
-			zarray=result.getJSONObject("ranges").getJSONObject(param).getJSONArray("values");
+			result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").remove("values");
+			result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").remove("values");
+			zarray=result.getJSONObject("ranges").getJSONObject(param).getJSONArray("values");			
 			String nodata="";
+			Double cellsize=1.;
+			Integer nxcols=null,nycols=null;
 			for(String line:literalValue.split(System.lineSeparator())) {
 				if(line.startsWith("[a-z]")) {
 					if(line.startsWith("ncols")) {
-						
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("num", Integer.valueOf(line.replace("ncols", "").trim()));
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("num", Integer.valueOf(line.replace("ncols", "").trim()));
+						nxcols=Integer.valueOf(line.replace("ncols", "").trim());
+						nycols=Integer.valueOf(line.replace("ncols", "").trim());
+					}
+					if(line.startsWith("xllcorner")) {
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("start", Double.valueOf(line.replace("xllcorner", "").trim()));
+											}
+					if(line.startsWith("yllcorner")) {
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("start", Double.valueOf(line.replace("yllcorner", "").trim()));
+					}
+					if(line.startsWith("cellsize")) {
+						cellsize=Double.valueOf(line.replace("cellsize", "").trim());						
 					}
 					if(line.startsWith("NODATA_value")) {
 						nodata=line.replace("NODATA_value", "").trim();
+					}
+					if(nxcols!=null && result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").has("start")) {
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("stop", Double.valueOf(line.replace("ncols", "").trim())+(cellsize*nxcols));
+					}
+					if(nycols!=null && result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").has("start")) {
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("stop", Double.valueOf(line.replace("ncols", "").trim())+(cellsize*nxcols));
 					}
 				}
 				for(String val:line.split(" ")) {
