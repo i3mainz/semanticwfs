1f4a171c044bd024134152e8d69453a71ee0e473
timo.homburg <timo.homburg@hs-mainz.de>
2020-09-20 21:45:43 +0200

    Prepared ASCII Grid format

---
+++ src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/coverage/ASCIIGridFormatter.java
@@ -0,0 +1,32 @@
+package de.hsmainz.cs.semgis.wfs.resultformatter.coverage;
+
+import java.util.List;
+
+import javax.xml.stream.XMLStreamException;
+
+import org.apache.jena.query.ResultSet;
+
+import de.hsmainz.cs.semgis.wfs.resultformatter.ResultFormatter;
+import de.hsmainz.cs.semgis.wfs.resultstyleformatter.StyleObject;
+
+public class ASCIIGridFormatter extends ResultFormatter {
+
+	public ASCIIGridFormatter() {
+		this.mimeType="text/asc";
+		this.exposedType="text/asc";
+		this.urlformat="asc";
+		this.label="ARC/INFO ASCII GRID Format (ASC)";
+		this.fileextension="asc";
+		this.definition="https://gdal.org/drivers/raster/xyz.html";
+	}
+	
+	@Override
+	public String formatter(ResultSet results, String startingElement, String featuretype, String propertytype,
+			String typeColumn, Boolean onlyproperty, Boolean onlyhits, String srsName, String indvar, String epsg,
+			List<String> eligiblenamespaces, List<String> noteligiblenamespaces, StyleObject mapstyle,
+			Boolean alternativeFormat, Boolean invertXY, Boolean coverage) throws XMLStreamException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
