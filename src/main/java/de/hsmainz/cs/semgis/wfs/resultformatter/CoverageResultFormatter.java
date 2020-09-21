8a4f2649d987cf2f48716928b3ef0f5ddd36bf69
timo.homburg <timo.homburg@hs-mainz.de>
2020-09-21 23:39:02 +0200

    CovJSON fix

--- src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/CoverageResultFormatter.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/CoverageResultFormatter.java
@@ -132,7 +132,7 @@ public abstract class CoverageResultFormatter extends ResultFormatter {
 			result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").remove("values");
 			zarray=result.getJSONObject("ranges").getJSONObject(param).getJSONArray("values");			
 			String nodata="";
-			Double cellsize=1.;
+			Double cellsize=1.,xllcorner=1.,yllcorner=1.;
 			Integer nxcols=null,nycols=null;
 			for(String line:literalValue.split(System.lineSeparator())) {
 					if(line.startsWith("ncols")) {
@@ -143,9 +143,11 @@ public abstract class CoverageResultFormatter extends ResultFormatter {
 					}
 					else if(line.startsWith("xllcorner")) {
 						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("start", Double.valueOf(line.replace("xllcorner", "").trim()));
+						xllcorner=Double.valueOf(line.replace("xllcorner", "").trim());
 											}
 					else if(line.startsWith("yllcorner")) {
 						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("start", Double.valueOf(line.replace("yllcorner", "").trim()));
+						yllcorner=Double.valueOf(line.replace("yllcorner", "").trim());
 					}
 					else if(line.startsWith("cellsize")) {
 						cellsize=Double.valueOf(line.replace("cellsize", "").trim());						
@@ -157,9 +159,9 @@ public abstract class CoverageResultFormatter extends ResultFormatter {
 						nodata=line.replace("NODATA_value", "").trim();				
 				}else {
 					if(nxcols!=null && result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").has("start")) 
-						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("stop", Double.valueOf(line.replace("ncols", "").trim())+(cellsize*nxcols));
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("x").put("stop", xllcorner+(cellsize*nxcols));
 					if(nycols!=null && result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").has("start")) 
-						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("stop", Double.valueOf(line.replace("ncols", "").trim())+(cellsize*nxcols));
+						result.getJSONObject("domain").getJSONObject("axes").getJSONObject("y").put("stop", yllcorner+(cellsize*nycols));
 				for(String val:line.split(" ")) {
 					if(val.equals(nodata)) {
 						zarray.put(JSONObject.NULL);
