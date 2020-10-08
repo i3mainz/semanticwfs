82cfe3b861da29da8db6eed388e7ba0a0f6bb850
timo.homburg <timo.homburg@hs-mainz.de>
2020-10-08 12:23:13 +0200

    Missing JS classes

---
+++ WebContent/config/js/evaluation.js
@@ -0,0 +1,12 @@
+var evaluation={
+		"percentage":{
+			"factor":1,
+			"usePriority":false,
+			"maxValue":0
+		},
+		"priorityPercentage":{
+			"factor":8,
+			"usePriority":true,
+			"maxValue":0
+		}
+}
---
+++ WebContent/config/js/evaluation_color_scheme.js
@@ -0,0 +1,6 @@
+var evaluation_color={
+	"percentage":[
+		{"title":"ScaledPercentage",{10:"darkred",30:"red",50:"orange",80:"green",100:"darkgreen"}}
+	]
+}
+			
