d9fc833d7bc65f19dd77ab8dd9a7172f923ad736
Timo <situx@users.noreply.github.com>
2023-06-14 00:32:10 +0200

    update

--- src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/rdf/TTLFormatter.java
+++ src/main/java/de/hsmainz/cs/semgis/wfs/resultformatter/rdf/TTLFormatter.java
@@ -92,7 +92,7 @@ public class TTLFormatter extends ResultFormatter {
 				}else if("lon".equalsIgnoreCase(name)){
 					lon=solu.get(name).toString();
 				}else {
-					if((val.size()-1)>0 && val.get(val.size()-1).startsWith("http") || val.get(val.size()-1).startsWith("file:/")) {
+					if((val.size()-1)>0 && (val.get(val.size()-1).startsWith("http") || val.get(val.size()-1).startsWith("file:/"))) {
 						builder.append("<"+solu.get(indvar)+"> <"+name+"> <"+solu.get(name).toString()+"> ."+System.lineSeparator());		
 					}else if((val.size()-1)>0 && val.get(val.size()-1).contains("^^")) {
 						builder.append("<"+solu.get(indvar)+"> <"+name+"> \""+solu.get(name).toString().substring(0,solu.get(name).toString().indexOf("^^"))+"\"^^<"+solu.get(name).toString().substring(solu.get(name).toString().indexOf("^^")+2)+"> ."+System.lineSeparator());
